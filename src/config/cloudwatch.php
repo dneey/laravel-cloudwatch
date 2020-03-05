3cdd64dca2e9c39a47fa42c665061978b20714ee
David Nee Yartey <yarteyd@itconsortiumgh.com>
2020-03-05 09:30:46 +0000

    updates

--- src/config/cloudwatch.php
+++ src/config/cloudwatch.php
@@ -8,12 +8,12 @@ return [
             'region' => env('AWS_CLOUD_WATCH_REGION', 'us-west-1'),
             'version' => env('AWS_CLOUD_WATCH_VERSION', 'latest'),
             'credentials' => [
-                'key' => env('AWS_CLOUD_WATCH_KEY_ID'),
-                'secret' => env('AWS_CLOUD_WATCH_SECRET'),
+                'key' => env('AWS_KEY'),
+                'secret' => env('AWS_SECRET'),
             ],
         ],
-        'stream_name' => env('AWS_CLOUD_WATCH_STREAM_NAME', env('APP_NAME') . '-' . config('APP_ENV')),
-        'group_name' => env('AWS_CLOUD_WATCH_GROUP_NAME', 'rpay'),
-        'retention' => env('AWS_CLOUD_WATCH_RETENTION_DAYS', 3),
-        'level' => env('AWS_CLOUD_WATCH_LEVEL', 'api'),
+        'stream_name' => env('CLOUD_WATCH_STREAM_NAME', env('APP_NAME')),
+        'group_name' => env('CLOUD_WATCH_GROUP_NAME', env('APP_NAME') . '-' . config('APP_ENV')),
+        'retention' => env('CLOUD_WATCH_RETENTION_DAYS', 14),
+        'level' => env('CLOUD_WATCH_LEVEL', 'api'),
     ]];
